94bbb5b9f723dcc46cebf7ea87f3b413eebfecce
leesi <joey@leesi.dev>
2023-10-10 19:35:01 -0400

    Code highlighting
    + Added error highlighting
    + Added example in Player.md
    + Updated normal highlight to have the lil bar on the left cause its cute

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,6 +103,18 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
         additionalLanguages: ["lua"],
+        magicComments: [
+          {
+            className: "code-block-highlighted-line",
+            line: "highlight-next-line",
+            block: { start: "highlight-start", end: "highlight-end" },
+          },
+          {
+            className: "code-block-error-line",
+            line: "error-next-line",
+            block: { start: "error-start", end: "error-end" },
+          },
+        ],
       },
     }),
   plugins: [
