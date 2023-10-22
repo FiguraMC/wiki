8b6140d1718239cceba436e4c2cf8e0c535c1d73
skyrina <sorryu02@gmail.com>
2023-10-22 17:29:08 +0300

    Deploy website - based on b5acac3b8b8017d54226abe0761ace517b362a90

--- assets/js/ca06c2e8.dd054538.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfigurawiki=self.webpackChunkfigurawiki||[]).push([[4049],{3769:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
