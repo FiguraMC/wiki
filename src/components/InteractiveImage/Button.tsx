039c9eb941dbf5c312dd1025e77609cf401d87bc
joeyleesi <joey@leesi.dev>
2023-11-07 20:20:24 -0500

    single quotes are better and we all know it

--- src/components/InteractiveImage/Button.tsx
+++ src/components/InteractiveImage/Button.tsx
@@ -1,6 +1,6 @@
-import styles from '@site/src/components/InteractiveImage/Button.module.css';
-import InteractiveImageContext from '@site/src/contexts/InteractiveImageContext';
-import React, { CSSProperties, FC, PropsWithChildren, useContext, useMemo, useState, useEffect } from 'react';
+import styles from "@site/src/components/InteractiveImage/Button.module.css";
+import InteractiveImageContext from "@site/src/contexts/InteractiveImageContext";
+import React, { CSSProperties, FC, PropsWithChildren, useContext, useMemo, useState, useEffect } from "react";
 
 export type InteractiveImageButtonType = PropsWithChildren<{
   id: string;
@@ -12,9 +12,16 @@ export type InteractiveImageButtonType = PropsWithChildren<{
   pulse?: boolean;
 }>;
 
-const InteractiveImageButton: FC<InteractiveImageButtonType> = (
-  { id, style, left, top, width, height, pulse, children },
-) => {
+const InteractiveImageButton: FC<InteractiveImageButtonType> = ({
+  id,
+  style,
+  left,
+  top,
+  width,
+  height,
+  pulse,
+  children,
+}) => {
   const [isHovered, setIsHovered] = useState(false);
   const { selected, setSelected } = useContext(InteractiveImageContext);
 
@@ -26,9 +33,9 @@ const InteractiveImageButton: FC<InteractiveImageButtonType> = (
       top,
       width,
       height,
-      position: 'absolute',
-      cursor: 'pointer',
-      background: '#fff',
+      position: "absolute",
+      cursor: "pointer",
+      background: "#fff",
       opacity: isSelected || isHovered ? 0.3 : 0.0,
     }),
     [isSelected, isHovered],
@@ -37,14 +44,14 @@ const InteractiveImageButton: FC<InteractiveImageButtonType> = (
   useEffect(() => {
     document
       .getAnimations()
-      .filter((a) => a.animationName === styles['pulse'])
+      .filter((a) => a.animationName === styles["pulse"])
       .forEach((a) => (a.startTime = 0));
   }, [isHovered]);
 
   return (
     <>
       <button
-        className={pulse && !isHovered && !isSelected ? styles['pulse'] : ''}
+        className={pulse && !isHovered && !isSelected ? styles["pulse"] : ""}
         onClick={() => setSelected(isSelected ? null : id)}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
