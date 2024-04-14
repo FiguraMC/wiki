8fef24e9bacf6d585b905eacfd8b7fc3f2ed0b03
joeyleesi <joey@leesi.dev>
2024-04-14 00:29:56 -0400

    forgor prettier

--- src/theme/DocCard/index.tsx
+++ src/theme/DocCard/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import DocCard from '@theme-original/DocCard';
-import type DocCardType from '@theme/DocCard';
-import type {WrapperProps} from '@docusaurus/types';
+import type { WrapperProps } from "@docusaurus/types";
+import DocCard from "@theme-original/DocCard";
+import type DocCardType from "@theme/DocCard";
+import React from "react";
 
 type Props = WrapperProps<typeof DocCardType>;
 
 export default function DocCardWrapper(props: Props): JSX.Element {
-  if (props.item.type == 'category' && props.item.customProps?.description) {
-    props.item.description = props.item.customProps.description
+  if (props.item.type == "category" && props.item.customProps?.description) {
+    props.item.description = props.item.customProps.description;
   }
   return (
     <>
